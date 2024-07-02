e5d46e5f330bee04faaf32c1f5150def86abbc8c
xiryss <59295280+xiryss@users.noreply.github.com>
2024-07-03 00:08:41 +0300

    Update Lcp.cpp

--- content/strings/Lcp.cpp
+++ content/strings/Lcp.cpp
@@ -1,4 +1,10 @@
 
+/**
+ * Author: Iurii Pustovalov
+ * Date: 2024-07-03
+ * Description: lcp array
+ * Time: O(n)
+ */
 vector<int> perm;
 vector<int> buildLCP(string &s, vector<int> &a) {
     int n = s.size();
@@ -19,13 +25,22 @@ vector<int> buildLCP(string &s, vector<int> &a) {
         while (s[i + cur] == s[j + cur]) cur++;
         lcp[ra[i]] = cur;
     }
-    // for suffixes!!!
-    s.pop_back();
-    a.erase(a.begin());
-    lcp.erase(lcp.begin());
     perm.resize(a.size());
     for (int i = 0; i < a.size(); ++i) perm[a[i]] = i;
     return lcp;
+}
+int cntr[MAXN];
+int spt[MAXN][lgg];
+void build(vector<int> &a) {
+    for (int i = 0; i < a.size(); ++i) {
+        spt[i][0] = a[i];
+    }
+    for (int i = 2; i < MAXN; ++i) cntr[i] = cntr[i / 2] + 1;
+    for (int h = 1; (1 << (h - 1)) < a.size(); ++h) {
+        for (int i = 0; i + (1 << (h - 1)) < a.size(); ++i) {
+            spt[i][h] = min(spt[i][h - 1], spt[i + (1 << (h - 1))][h - 1]);
+        }
+    }
 }
 int getLCP(int l, int r) {
     l = perm[l], r = perm[r];
